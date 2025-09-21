1d6e34c00ff0e72329f0b08652023f74e02d3347
Aksinya310 <aksinya.serdyukova@mail.ru>
2025-09-21 18:16:17 +0300

    функция для корректного ввода чисел

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -19,3 +19,13 @@ struct CS
 	int in_work;
 	double effectiveness;
 }; 
+template <typename T>
+T GetCorrectNumber(T min, T max) {
+	int x;
+	while ((cin >> x).fail() || cin.peek() != '\n' || x < min || x > max) {
+		cin.clear();
+		cin.ignore(1000, '\n');
+		cout << "\nДанные введены неверно" << endl;
+	}
+	return x;
+}
