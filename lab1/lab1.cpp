44b37186138892f9c9630fb43ae029b086b9502f
Aksinya310 <aksinya.serdyukova@mail.ru>
2025-09-21 20:39:58 +0300

    функция для меню

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 struct Pipe
 {
-	string name1;
+	string name;
 	double length;
 	double diameter;
 	bool sign;
@@ -14,7 +14,7 @@ struct Pipe
 
 struct CS
 {
-	string name2; 
+	string name; 
 	int number_workshop;
 	int in_work;
 	double effectiveness;
@@ -28,4 +28,25 @@ T GetCorrectNumber(T min, T max) {
 		cout << "\nДанные введены неверно" << endl;
 	}
 	return x;
+}
+int menu()
+{
+	int number;
+	while (true)
+	{
+		cout << "Выберите пункт из меню" << endl;
+		cout << "Меню:" << endl;
+		cout << "1. Добавить трубу" << endl;
+		cout << "2. Добавить КС" << endl;
+		cout << "3. Просмотр всех объектов" << endl;
+		cout << "4. Редактировать трубу" << endl;
+		cout << "5. Редактировать КС" << endl;
+		cout << "6. Сохранить" << endl;
+		cout << "7. Загрузить" << endl;
+		cout << "0. Выход" << endl;
+		number = GetCorrectNumber(0, 7);
+		cout << "\n";
+		break;
+	}
+	return number;
 }
